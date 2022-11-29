99cce41a72e74a7a55179400409ed5d9968dc376
dmitry <rbdeman@yandex.ru>
2022-11-29 21:56:08 +0300

    Сделал первую задачу

--- Zadacha_one/Program.cs
+++ Zadacha_one/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("введи пятизначное число: ");
+int number = int.Parse(Console.ReadLine());
+int numberOne = number / 10000;
+int numberTwo = number / 1000 % 10;
+int numberFour = number / 10 % 10;
+int numberFive = number % 10;
+if ((number<10000) || (number>99999)) // проверка корректности ввода
+{
+    Console.WriteLine("Некорректный ввод числа");
+}
+else
+{
+    if ((numberOne == numberFive) && (numberTwo == numberFour))
+    {
+        Console.WriteLine("Данное число-палиндром");
+    }
+    else
+    {
+        Console.WriteLine("Данное число-не палиндром");
+    }
+}
