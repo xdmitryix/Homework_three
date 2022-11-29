55d749745142872beedb5c036c8b03f559e2b7c1
dmitry <rbdeman@yandex.ru>
2022-11-29 22:21:10 +0300

    сделал третью задачу

--- Zadacha_three/Program.cs
+++ Zadacha_three/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+int DataEntry(string str)
+{
+    Console.Write(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int count = 1;
+int number = DataEntry("введи число: ");
+while (count < number + 1)
+{
+    double result = Math.Pow(count, 3);
+    if (count == number)
+    {
+        Console.Write($"{result}");
+    }
+    else
+    {
+        Console.Write($"{result},");
+    }
+    count++;
+}
